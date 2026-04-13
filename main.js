3150c576e211ab0d128155eeac9ce8c4c621d7fe
B-3-N-J-4-M-1-N <hcq513@york.ac.uk>
2026-04-13 17:30:43 +0100

    bing bong

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const savedChoice = localStorage.getItem("themeChoice");
+
 let lightTheme = {
     background: "#f4f3ecff",
     textColour: "#000000",
@@ -15,3 +17,14 @@ let jamaicanTheme = {
     textColour: "#1b3107ff",
     font: "open-sans"
 }
+
+let currentTheme = document.getElementById("displayMode");
+console.log("currentTheme:", currentTheme.value);
+
+if (savedChoice !== null) {
+    currentTheme.value = savedChoice;
+    console.log("savedChoice:", savedChoice);
+}
+
+const choice = currentTheme.value;
+localStorage.setItem("themeChoice", choice);
